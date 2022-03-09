8ae44e642a417a4573c79364b761520d71477b1b
Alexander Streed <desertaxle@users.noreply.github.com>
2022-03-09 13:50:53 -0600

    Adds `create_secret` and `delete_secret` tasks (#13)
    * Add create_secret and delete_secret tests
    
    * Adds Python 3.10 to Run Tests job

--- tests/test_secrets_manager.py
+++ tests/test_secrets_manager.py
@@ -1,9 +1,16 @@
+from datetime import date, timedelta
+
 import boto3
 import pytest
 from moto import mock_secretsmanager
 from prefect import flow
 
-from prefect_aws.secrets_manager import read_secret, update_secret
+from prefect_aws.secrets_manager import (
+    create_secret,
+    delete_secret,
+    read_secret,
+    update_secret,
+)
 
 
 @pytest.fixture
@@ -61,7 +68,7 @@ async def test_read_secret(secret_under_test, aws_credentials):
     assert (await test_flow()).result().result() == expected_value
 
 
-async def test_update_secret(secret_under_test, aws_credentials):
+async def test_update_secret(secret_under_test, aws_credentials, secretsmanager_client):
     current_secret_value = secret_under_test["expected_value"]
     new_secret_value = (
         current_secret_value + "2"
@@ -80,11 +87,78 @@ async def test_update_secret(secret_under_test, aws_credentials):
     flow_state = await test_flow()
     assert flow_state.result().result().get("Name") == secret_under_test["secret_name"]
 
-    sm_client = aws_credentials.get_boto3_session().client("secretsmanager")
-    updated_secret = sm_client.get_secret_value(
+    updated_secret = secretsmanager_client.get_secret_value(
         SecretId=secret_under_test["secret_name"]
     )
     assert (
         updated_secret.get("SecretString") == new_secret_value
         or updated_secret.get("SecretBinary") == new_secret_value
     )
+
+
+@pytest.mark.parametrize(
+    ["secret_name", "secret_value"], [["string_secret", "42"], ["binary_secret", b"42"]]
+)
+async def test_create_secret(
+    aws_credentials, secret_name, secret_value, secretsmanager_client
+):
+    @flow
+    async def test_flow():
+        return await create_secret(
+            secret_name=secret_name,
+            secret_value=secret_value,
+            aws_credentials=aws_credentials,
+        )
+
+    flow_state = await test_flow()
+    assert flow_state.result().result().get("Name") == secret_name
+
+    updated_secret = secretsmanager_client.get_secret_value(SecretId=secret_name)
+    assert (
+        updated_secret.get("SecretString") == secret_value
+        or updated_secret.get("SecretBinary") == secret_value
+    )
+
+
+@pytest.mark.parametrize(
+    ["recovery_window_in_days", "force_delete_without_recovery"],
+    [
+        [30, False],
+        [90, False],
+        [7, False],
+        [6, False],
+        [10, False],
+        [15, True],
+        [31, True],
+    ],
+)
+async def test_delete_secret(
+    aws_credentials,
+    secret_under_test,
+    recovery_window_in_days,
+    force_delete_without_recovery,
+):
+    @flow
+    async def test_flow():
+        return await delete_secret(
+            secret_name=secret_under_test["secret_name"],
+            aws_credentials=aws_credentials,
+            recovery_window_in_days=recovery_window_in_days,
+            force_delete_without_recovery=force_delete_without_recovery,
+        )
+
+    flow_state = await test_flow()
+    if not force_delete_without_recovery and not 7 <= recovery_window_in_days <= 30:
+        with pytest.raises(ValueError):
+            result = flow_state.result().result()
+
+    else:
+        result = flow_state.result().result()
+        assert result.get("Name") == secret_under_test["secret_name"]
+        deletion_date = result.get("DeletionDate")
+        if not force_delete_without_recovery:
+            assert deletion_date.date() == (
+                date.today() + timedelta(days=recovery_window_in_days)
+            )
+        else:
+            assert deletion_date.date() == date.today()
