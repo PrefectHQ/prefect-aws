797f814344e4313dfc387f0f7531da1074dc947b
ahuang11 <andrew.h@prefect.io>
2022-06-24 11:50:21 -0700

    Add back harness

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import pytest
+from prefect.testing.utilities import prefect_test_harness
 
 from prefect_aws import AwsCredentials
 from prefect_aws.client_parameters import AwsClientParameters
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prefect_db():
+    with prefect_test_harness():
+        yield
 
 
 @pytest.fixture
