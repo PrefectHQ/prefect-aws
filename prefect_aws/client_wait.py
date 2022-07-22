8ee2c7bc2eec8246438a7afb6116e96c1cb6fb9c
Matt Conger <mtconger97@gmail.com>
2022-07-22 11:50:36 -0700

    updates from morning meeting

--- prefect_aws/client_wait.py
+++ prefect_aws/client_wait.py
@@ -7,60 +7,77 @@ try:
 except ImportError:
     import importlib_resources as pkg_resources
 
-import boto3
-from botocore.exceptions import WaiterError
-from botocore.waiter import WaiterModel, create_waiter_with_client
-
-
 from functools import partial
 from typing import Any, Dict, Optional
 
+import boto3
 from anyio import to_thread
+from botocore.exceptions import WaiterError
+from botocore.waiter import WaiterModel, create_waiter_with_client,Waiter
+from prefect import get_run_logger, task
 
-from prefect_aws.credentials import AwsCredentials
 from prefect_aws import waiters
-from prefect import task, get_run_logger 
+from prefect_aws.credentials import AwsCredentials
 
 
 @task
 async def client_waiter(
     client: str,
     waiter_name: str,
-    waiter_definition: Dict[str, Any],
     aws_credentials: AwsCredentials,
+    waiter_definition: Optional[Dict[str, Any]] = None,
     **waiter_kwargs: Optional[Dict[str, Any]],
 ):
     """
     Uses the underlying boto3 waiter functionality.
 
     Args:
-        client: The AWS client on which to wait (e.g., 'batch', 'ec2', etc).
+        client: The AWS client on which to wait (e.g., 'client_wait', 'ec2', etc).
         waiter_name: The name of the waiter to instantiate. Can be a boto-supported
             waiter or one of prefect's custom waiters. Currently, Prefect offers three additional
-            waiters for AWS Batch: `"JobExists"` waits for a job to be instantiated, `"JobRunning"`
+            waiters for AWS client_wait: `"JobExists"` waits for a job to be instantiated, `"JobRunning"`
             waits for a job to start running, and `"JobComplete"` waits for a job to finish. You can
             find the definitions for all prefect-defined waiters [here](
             https://github.com/PrefectHQ/prefect-aws/tree/master/prefect_aws/waiters/).
             You may also use a custom waiter name, if you supply an accompanying waiter definition
             dict.
-        waiter_definition: A valid custom waiter model, as a dict. Note that if
-            you supply a custom definition, it is assumed that the provided 'waiter_name' is
-            contained within the waiter definition dict.
         credentials: your AWS credentials passed from an upstream
             Secret task; this Secret must be a JSON string
             with two keys: `ACCESS_KEY` and `SECRET_ACCESS_KEY` which will be
             passed directly to `boto3`.  If not provided here or in context, `boto3`
             will fall back on standard AWS rules for authentication.
+        waiter_definition: A valid custom waiter model, as a dict. Note that if
+            you supply a custom definition, it is assumed that the provided 'waiter_name' is
+            contained within the waiter definition dict.
         waiter_kwargs: Arguments to pass to the `waiter.wait(...)` method. Will
             depend upon the specific waiter being called.
-    
-    Examples:
-        TODO:
+
+    Example:
+        Run an ec2 waiter until instance_exists.
+        ```python
+        from prefect import flow
+        from prefect_aws import AwsCredentials
+        from prefect_aws.client_wait import client_waiter
+
+        @flow
+        def example_client_wait_flow():
+            aws_credentials = AwsCredentials(
+                aws_access_key_id="acccess_key_id",
+                aws_secret_access_key="secret_access_key"
+            )
+
+            waiter = client_waiter(
+                "ec2",
+                "instance_exists",
+                aws_credentials
+            )
+
+            return waiter 
+        example_client_wait_flow()
+        ```
     """
     logger = get_run_logger()
-    logger.info("Waiting on %s job", client)        
-
-    waiter_kwargs = waiter_kwargs or {}
+    logger.info("Waiting on %s job", client)
 
     boto_client = aws_credentials.get_boto3_session().client(client)
 
@@ -80,8 +97,11 @@ async def client_waiter(
 
 
 def _load_prefect_waiter(
-    boto_client: boto3.client, client_str: str, waiter_name: str
-):
+        boto_client: boto3.client, 
+        client_str: str, 
+        waiter_name: str,
+    ) -> Waiter:
+    
     """
     Load a custom waiter from the ./waiters directory.
     """
@@ -89,3 +109,4 @@ def _load_prefect_waiter(
     with pkg_resources.open_text(waiters, f"{client_str}.json") as handle:
         waiter_model = WaiterModel(json.load(handle))
     return create_waiter_with_client(waiter_name, waiter_model, boto_client)
+ 
--- tests/test_client_wait.py
+++ tests/test_client_wait.py
@@ -1,19 +1,67 @@
-from prefect import task, flow
+from unittest.mock import MagicMock
 
 import boto3
 import pytest
-from unittest.mock import MagicMock
 from moto import mock_batch, mock_iam
-from prefect import flow
+from prefect import flow, task
 
-from prefect_aws.client_wait import _load_prefect_waiter
+from botocore.waiter import WaiterModel
+from prefect_aws.credentials import AwsCredentials
+
+from prefect_aws.client_wait import _load_prefect_waiter, client_waiter
+
+
+TEST_WAITERS = {
+    "version": 2,
+    "waiters": {
+        "TestWaiter": {
+            "delay": 5,
+            "operation": "DescribeJobs",
+            "maxAttempts": 100,
+            "acceptors": [
+                {
+                    "argument": "jobs[].status",
+                    "expected": "FAILED",
+                    "matcher": "pathAll",
+                    "state": "success",
+                },
+                {
+                    "argument": "jobs[].status",
+                    "expected": "SUCCEEDED",
+                    "matcher": "pathAll",
+                    "state": "success",
+                },
+            ],
+        }
+    },
+}
+
 
 def mock_create_waiter_with_client(*args, **kwargs):
-    return MagicMock()
+    return "waiter"
+
+def mock_waiter_wait(*args,**kwargs):
+    return "wait1"
+
 
 def test_load_prefect_waiter(monkeypatch):
-    monkeypatch.setattr("botocore.waiter.create_waiter_with_client", mock_create_waiter_with_client)
-
+    monkeypatch.setattr(
+        "prefect_aws.client_wait.create_waiter_with_client",
+        mock_create_waiter_with_client,
+    )
     mock_boto_client = MagicMock()
     result = _load_prefect_waiter(mock_boto_client, "batch", "JobExists")
-    assert result
+    assert result == "waiter"
+
+def test_client_waiter(monkeypatch, aws_credentials):
+    monkeypatch.setattr(
+        'boto3.waiter.EC2.Waiter.InstanceExists.wait',
+        mock_waiter_wait
+    )
+
+    waiter = client_waiter(
+        "ec2",
+        "instance_exists",
+        aws_credentials)
+    
+    assert waiter is None
