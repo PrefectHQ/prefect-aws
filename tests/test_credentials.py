73e2ac2c4b42f5d953f23a3ace73ca6edd291f4a
Serina Grill <serinagrill@gmail.com>
2022-07-25 14:47:31 -0700

    Access conftest.py in test_credentials

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -8,21 +8,10 @@ from botocore.client import ClientError
 BUCKET_NAME = "MY_BUCKET"
 
 
-@pytest.fixture()
-def mock_creds():
-
-    """Mock AWS credentials for moto."""
-
-    moto_credentials_filepath = (
-        Path(__file__).parent.absolute() / "mock_aws_credentials"
-    )
-    os.environ["AWS_SHARED_CREDENTIALS_FILE"] = str(moto_credentials_filepath)
-
-
 @pytest.mark.parametrize(
     argnames="profile_nm", argvalues=[("TEST_PROFILE_1"), ("TEST_PROFILE_2")]
 )
-def test_get_s3_client(mock_creds, profile_nm: str):
+def test_get_s3_client(aws_credentials, profile_nm: str):
 
     """
     Given an AWS profile name, will create an AwsCredentials block and return an S3 Client."""
@@ -36,7 +25,7 @@ def test_get_s3_client(mock_creds, profile_nm: str):
 @pytest.mark.parametrize(
     argnames="profile_nm", argvalues=[("TEST_PROFILE_1"), ("TEST_PROFILE_2")]
 )
-def test_create_bucket_and_return_location(mock_creds, profile_nm: str) -> dict:
+def test_create_bucket_and_return_location(aws_credentials, profile_nm: str) -> dict:
 
     """
     Given an S3 client generated from the AwsCredentials block, creates bucket
