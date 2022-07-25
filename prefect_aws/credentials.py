88a6bf99c496c6cfa7755264fd36bdebd5d58042
Serina Grill <serinagrill@gmail.com>
2022-07-25 14:58:08 -0700

    Retrieve value from aws_secret_access_key SecretStr

--- prefect_aws/credentials.py
+++ prefect_aws/credentials.py
@@ -53,9 +53,10 @@ class AwsCredentials(Block):
         """
         return boto3.Session(
             aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
+            aws_secret_access_key=self.aws_secret_access_key.get_secret_value(),
             aws_session_token=self.aws_session_token,
             profile_name=self.profile_name,
             region_name=self.region_name,
         )
+
 
