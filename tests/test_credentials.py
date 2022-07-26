5020395d2481c1415dde5317018f7b319870d472
Serina Grill <serinagrill@gmail.com>
2022-07-25 23:12:02 -0700

    Create simple test to get boto3 session

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,40 +1,16 @@
-from botocore.client import ClientError
 from moto import mock_s3
+from boto3.session import Session
 
 from prefect_aws.credentials import AwsCredentials
 
-BUCKET_NAME = "MY_BUCKET"
 
-
-def test_get_s3_client(aws_credentials):
+def test_aws_credentials_get_boto3_session(aws_credentials):
 
     """
-    Given an AWS profile name, will create an AwsCredentials block and return
-    an S3 Client."""
+    Asserts that instantiated AwsCredentials block creates an 
+    authenticated boto3 session."""
 
     with mock_s3():
         aws_credentials_block = AwsCredentials()
-        s3_client = aws_credentials_block.get_boto3_session().client("s3")
-        return s3_client
-
-
-def test_create_bucket_and_return_location(aws_credentials) -> dict:
-
-    """
-    Given an S3 client generated from the AwsCredentials block, creates bucket
-    and validates existence. If not exists will raise an exception.
-
-    Called during testing as part of assertion that session is properly
-    configured from instantiated AwsCredentials block.
-    """
-
-    with mock_s3():
-        aws_credentials_block = AwsCredentials()
-        s3_client = aws_credentials_block.get_boto3_session().client("s3")
-        s3_client.create_bucket(Bucket=BUCKET_NAME)
-
-        try:
-            return s3_client.get_bucket_location(Bucket=BUCKET_NAME)
-
-        except ClientError:
-            raise Exception("Bucket was not created.")
+        boto3_session = aws_credentials_block.get_boto3_session()
+        assert isinstance(boto3_session, Session)
