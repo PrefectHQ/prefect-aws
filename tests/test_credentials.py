51e5cc6cb6f714724c4aa56e6ad9994f0dbe5511
Serina Grill <serinagrill@gmail.com>
2022-07-25 23:17:09 -0700

    Reformat docstring

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,13 +1,15 @@
 from boto3.session import Session
 from moto import mock_s3
+
 from prefect_aws.credentials import AwsCredentials
 
 
 def test_aws_credentials_get_boto3_session(aws_credentials):
 
     """
-    Asserts that instantiated AwsCredentials block creates an 
-    authenticated boto3 session."""
+    Asserts that instantiated AwsCredentials block creates an
+    authenticated boto3 session.
+    """
 
     with mock_s3():
         aws_credentials_block = AwsCredentials()
