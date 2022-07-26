a87737d1ffbcf3d8d89a4202dae5ec1f24b58d68
Serina Grill <serinagrill@gmail.com>
2022-07-25 17:09:39 -0700

    Handle aws_secret_access_key default value of None

--- prefect_aws/credentials.py
+++ prefect_aws/credentials.py
@@ -57,10 +57,16 @@ class AwsCredentials(Block):
             >>> )
             >>> s3_client = aws_credentials.get_boto3_session().client("s3")
         """
-        return boto3.Session(
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key.get_secret_value(),
-            aws_session_token=self.aws_session_token,
-            profile_name=self.profile_name,
-            region_name=self.region_name,
-        )
+
+        if self.aws_secret_access_key:
+            aws_secret_access_key = self.aws_secret_access_key.get_secret_value()
+        else:
+            aws_secret_access_key = None
+
+            return boto3.Session(
+                aws_access_key_id=self.aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=self.aws_session_token,
+                profile_name=self.profile_name,
+                region_name=self.region_name,
+            )
