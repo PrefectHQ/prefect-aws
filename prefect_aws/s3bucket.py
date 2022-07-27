d7ee68566b4f4d1caacbcee7dd16314122106c38
Serina Grill <serinagrill@gmail.com>
2022-07-26 22:39:53 -0700

    Test commit

--- prefect_aws/s3bucket.py
+++ prefect_aws/s3bucket.py
@@ -1,35 +1,46 @@
 import io
-from prefect.filesystems import ReadableFileSystem, WritableFileSystem
-from pyparsing import Optional
-from prefect_aws import AwsClientParameters, AwsCredentials
+from prefect.filesystems import ReadableFileSystem, WritableFileSystem, RemoteFileSystem
+from typing import Optional
+import urllib3
+from prefect_aws import AwsCredentials
+from pydantic import HttpUrl, SecretStr
+from prefect.blocks.core import Block
+from typing import Union
+import boto3
 
 class S3Bucket(ReadableFileSystem, WritableFileSystem):
+
     """Info here."""
+
     bucket: str
-    key: str 
-    credentials: AwsCredentials # default but could specify other credentials, i.e MinIO
-    base_path: Optional[str] = None
+    # make this secret
+    credentials: Union[AwsCredentials, MinIOCredentials]
+    basepath: Optional[str] = None
     endpoint_url: Optional[str] = None
 
-    def block_initialization(self) -> None:
-        self.aws_session = self.credentials.get_boto3_session()
+    # change
+    _logo_url: Optional[HttpUrl] = "https://miro.medium.com/max/1024/0*PGkPVIgj0M5hF92Y.png"  # noqa
 
-    def check_basepath():
-        pass
-
-    def _resolve_path():
-        pass
-
-    def read_path(self, path) -> bytes:
-        resolved_path = self._resolve_path(path)
-        s3_client = self.aws_session.client('s3', endpoint_url=self.endpoint_url)
+    def read_path(self, path: str) -> bytes:
+        s3_client = self.credentials.get_boto3_session().client('s3', endpoint_url=self.endpoint_url)
         with io.BytesIO() as stream:
-            s3_client.download_fileobj(Bucket=self.bucket, Key=self.key, FileObj=stream)
+            s3_client.download_fileobj(
+                Bucket=self.bucket,
+                Key=self.basepath,
+                FileObj=stream
+                )
             stream.seek(0)
             output = stream.read()
             return output
 
     def write_path(self, path: str, content: bytes) -> None:
-        s3_client = self.aws_session.client('s3', endpoint_url=self.endpoint_url)
+        if self.endpoint_url:
+            s3_client = self.credentials.get_boto3_session().client('s3', endpoint_url=self.endpoint_url)
+        else:
+            s3_client = self.credentials.get_boto3_session().client('s3')
         with io.BytesIO(content) as stream:
-            s3_client.upload_fileobj(Bucket=self.bucket, Key=self.key, FileObj=stream)
+            s3_client.upload_fileobj(
+                Fileobj=stream,
+                Bucket=self.bucket,
+                Key=self.basepath  # could fail if none?
+                )
