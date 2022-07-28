c843f9923371b41ac7843640fa59f2fe74cf1e69
Serina Grill <serinagrill@gmail.com>
2022-07-28 13:51:34 -0700

    Add basepath func test

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -5,13 +5,16 @@ from moto import mock_s3
 
 from prefect_aws import AwsCredentials, MinIOCredentials, S3Bucket
 
-aws_creds_block = AwsCredentials(
-    aws_access_key_id="testing", aws_secret_access_key="testing"
-)
 
-minio_creds_block = MinIOCredentials(
-    minio_root_user="testing", minio_root_password="testing"
-)
+@pytest.fixture
+def aws_creds_block():
+    return AwsCredentials(aws_access_key_id="testing", aws_secret_access_key="testing")
+
+
+@pytest.fixture
+def minio_creds_block():
+    return MinIOCredentials(minio_root_user="testing", minio_root_password="testing")
+
 
 bucket_name = "test_bucket"
 
@@ -83,3 +86,22 @@ async def test_read_fails_does_not_exist(s3, creds):
     )
     with pytest.raises(ClientError):
         await fs.read_path("test_bucket/foo/bar")
+
+
+@pytest.mark.parametrize("creds", [aws_creds_block, minio_creds_block])
+async def test_basepath(s3, creds):
+
+    """
+    Create an S3 bucket, instantiate S3Bucket block, write to and read from
+    bucket.
+    """
+
+    s3.create_bucket(Bucket=bucket_name)
+
+    fs = S3Bucket(
+        bucket_name=bucket_name,
+        credentials=creds,
+        basepath="subfolder"
+    )
+    key = await fs.write_path("test.txt", content=b"hello")
+    assert await fs.read_path(key) == b"hello"
