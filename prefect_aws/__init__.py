fa427d43666651a24c76352b544d06b43cae101f
Serina Grill <serinagrill@gmail.com>
2022-07-27 17:38:37 -0700

    Expose S3Bucket in __init__

--- prefect_aws/__init__.py
+++ prefect_aws/__init__.py
@@ -1,7 +1,8 @@
 from . import _version
-from .credentials import AwsCredentials
+from .credentials import AwsCredentials, MinIOCredentials
 from .client_parameters import AwsClientParameters
+from .s3bucket import S3Bucket
 
-__all__ = ["AwsCredentials", "AwsClientParameters"]
+__all__ = ["AwsCredentials", "AwsClientParameters", "MinIOCredentials", "S3Bucket"]
 
 __version__ = _version.get_versions()["version"]
