aa402579f905ea0f97e9635b187ad05354a906e6
Serina Grill <serinagrill@gmail.com>
2022-07-28 16:59:05 -0700

    Updating tests

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -2,7 +2,6 @@ import boto3
 import pytest
 from botocore.exceptions import ClientError
 from moto import mock_s3
-
 from prefect_aws import AwsCredentials, MinIOCredentials, S3Bucket
 
 
@@ -13,7 +12,10 @@ def aws_creds_block():
 
 @pytest.fixture
 def minio_creds_block():
-    return MinIOCredentials(minio_root_user="testing", minio_root_password="testing")
+    return MinIOCredentials(
+        minio_root_user="minioadmin",
+        minio_root_password="minioadmin"
+    )
 
 
 bucket_name = "test_bucket"
@@ -25,66 +27,74 @@ def s3():
     """Mock connection to AWS S3 with boto3 client."""
 
     with mock_s3():
+
         yield boto3.client(
             service_name="s3",
             region_name="us-east-1",
-            aws_access_key_id="testing",
+            aws_access_key_id="minioadmin",
             aws_secret_access_key="testing",
             aws_session_token="testing",
         )
 
 
-@pytest.mark.parametrize("creds", [aws_creds_block, minio_creds_block])
-async def test_read_write_roundtrip(s3, creds):
+@pytest.fixture(params=["aws_credentials", "minio_credentials"])
+def s3_bucket(s3, request, aws_creds_block, minio_creds_block):
+
+    key = request.param
+
+    if key == "aws_credentials":
+        fs = S3Bucket(bucket_name=bucket_name, aws_credentials=aws_creds_block)
+    elif key == "minio_credentials":
+        fs = S3Bucket(bucket_name=bucket_name, minio_credentials=minio_creds_block)
+
+    s3.create_bucket(Bucket=bucket_name)
+
+    return fs
+
+
+async def test_read_write_roundtrip(s3_bucket):
 
     """
     Create an S3 bucket, instantiate S3Bucket block, write to and read from
     bucket.
     """
 
-    s3.create_bucket(Bucket=bucket_name)
-    fs = S3Bucket(bucket_name=bucket_name, credentials=creds)
-    key = await fs.write_path("test.txt", content=b"hello")
-    assert await fs.read_path(key) == b"hello"
+    key = await s3_bucket.write_path("test.txt", content=b"hello")
+    assert await s3_bucket.read_path(key) == b"hello"
 
 
-@pytest.mark.parametrize("creds", [aws_creds_block, minio_creds_block])
-async def test_write_with_missing_directory_succeeds(s3, creds):
+async def test_write_with_missing_directory_succeeds(s3_bucket):
 
     """
     Create an S3 bucket, instantiate S3Bucket block, write to path with
     missing directory.
     """
 
-    s3.create_bucket(Bucket=bucket_name)
-    fs = S3Bucket(bucket_name=bucket_name, credentials=creds)
-    key = await fs.write_path("folder/test.txt", content=b"hello")
-    assert await fs.read_path(key) == b"hello"
+    key = await s3_bucket.write_path("folder/test.txt", content=b"hello")
+    assert await s3_bucket.read_path(key) == b"hello"
 
 
-@pytest.mark.parametrize("creds", [aws_creds_block, minio_creds_block])
-async def test_read_fails_does_not_exist(s3, creds):
+async def test_read_fails_does_not_exist(s3_bucket):
 
     """
     Create an S3 bucket, instantiate S3Bucket block, assert read from
     nonexistent path fails.
     """
 
-    s3.create_bucket(Bucket=bucket_name)
-    fs = S3Bucket(bucket_name=bucket_name, credentials=creds)
     with pytest.raises(ClientError):
-        await fs.read_path("test_bucket/foo/bar")
+        await s3_bucket.read_path("test_bucket/foo/bar")
 
 
-@pytest.mark.parametrize("creds", [aws_creds_block, minio_creds_block])
-async def test_basepath(s3, creds):
+async def test_aws_basepath(s3_bucket, aws_creds_block):
 
-    """
-    Create an S3 bucket, instantiate S3Bucket block, write to and read from
-    bucket.
-    """
+    """Test the basepath functionality."""
 
-    s3.create_bucket(Bucket=bucket_name)
-    fs = S3Bucket(bucket_name=bucket_name, credentials=creds, basepath="subfolder")
-    key = await fs.write_path("test.txt", content=b"hello")
-    assert await fs.read_path(key) == b"hello"
+    # create a new block with a subfolder
+    s3_bucket_block = S3Bucket(
+        bucket_name=bucket_name,
+        aws_credentials=aws_creds_block,
+        basepath="subfolder",
+    )
+
+    key = await s3_bucket_block.write_path("data.csv", content=b"hello")
+    assert await s3_bucket_block.read_path(key) == b"hello"
