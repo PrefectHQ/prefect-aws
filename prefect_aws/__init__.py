238620d3d3c2b41fc517e6046c032f31901fef74
Serina Grill <serinagrill@gmail.com>
2022-07-30 21:40:57 -0400

    Remove S3Bucket from root module

--- prefect_aws/__init__.py
+++ prefect_aws/__init__.py
@@ -1,8 +1,7 @@
 from . import _version
 from .credentials import AwsCredentials, MinIOCredentials
 from .client_parameters import AwsClientParameters
-from .filesystems import S3Bucket
 
-__all__ = ["AwsCredentials", "AwsClientParameters", "MinIOCredentials", "S3Bucket"]
+__all__ = ["AwsCredentials", "AwsClientParameters", "MinIOCredentials"]
 
 __version__ = _version.get_versions()["version"]
