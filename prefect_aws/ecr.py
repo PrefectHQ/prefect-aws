5eb98fdbabc4020ce296b0ab78c7946705c30a73
Michael Adkins <michael@prefect.io>
2022-08-10 15:55:43 -0500

    Add docstrings

--- prefect_aws/ecr.py
+++ prefect_aws/ecr.py
@@ -10,7 +10,6 @@ from prefect_aws import AwsCredentials
 
 
 class ElasticContainerRegistry(BaseDockerLogin):
-
     """
     Block to login to ECR registries.
     """
@@ -26,9 +25,15 @@ class ElasticContainerRegistry(BaseDockerLogin):
 
     @sync_compatible
     async def login(self):
+        """
+        Log in to the configured ECR registry.
+
+        Credentials will be persisted to the `docker login` default.
+        """
         return await run_sync_in_worker_thread(self._login_sync)
 
     def _login_sync(self):
+        """Sync implementation of login"""
         token, registry_url = self._get_token_and_endpoint()
         username, password = self._parse_token(token)
         # Use the base implementation to perform login
@@ -44,10 +49,23 @@ class ElasticContainerRegistry(BaseDockerLogin):
         username, password = decoded_token.split(sep=":", maxsplit=1)
         return username, password
 
-    def _get_client(self):
+    def _get_ecr_client(self):
+        """
+        Get an Boto3 ECR client.
+
+        Note: A different client will be necessary for public-ECR
+        """
         return self.aws_credentials.get_boto3_session().client(service_name="ecr")
 
     def _get_token_and_endpoint(self):
+        """
+        Retrieve an unexpired ECR token and endpoint URL.
+
+        Each token retrieved is cached with its expiration date. A new token only
+        retrieved when the cached token expires.
+
+        ECR tokens generally expire after 12 hours.
+        """
         if (
             self._cached_token
             and datetime.now(tz=self._token_expiration.tzinfo) < self._token_expiration
@@ -55,7 +73,7 @@ class ElasticContainerRegistry(BaseDockerLogin):
             # Return the cached token if it has not expired
             return self._cached_token, self._registry_url
 
-        client = self._get_client()
+        client = self._get_ecr_client()
 
         registry_id = self.registry_id or client.describe_registry()["registryId"]
 
