0973f2762df995a872ba11d1bc60076efd993b06
Michael Adkins <michael@prefect.io>
2022-08-16 13:19:17 -0500

    Privatize functions and clean up unused code

--- prefect_aws/ecs.py
+++ prefect_aws/ecs.py
@@ -19,7 +19,9 @@ ECS_TASK_RUN_CONTAINER_NAME = "prefect"
 
 
 class ECSTask(Infrastructure):
-    """ """
+    """
+    Run a command as an ECS task
+    """
 
     type: Literal["ecs-task"] = "ecs-task"
 
@@ -29,10 +31,6 @@ class ECSTask(Infrastructure):
     task_definition_arn: Optional[str] = None
     task_definition: Optional[dict] = None
     image: str = Field(default_factory=get_prefect_image_name)
-
-    # TODO: Determine if these should be included
-    # task_family: Optional[str] = None
-    # network_mode: Optional[str] = None
 
     # Task run settings
     launch_type: Optional[Literal["FARGATE", "EC2", "EXTERNAL"]] = None
@@ -49,7 +47,7 @@ class ECSTask(Infrastructure):
     def set_default_launch_type(cls, values):
         """
         To use FARGATE_SPOT, the launch type must be left empty. If a launch type is
-        not providing and the user isn't using FARGATE_SPOT, the launch type defaults
+        not provided and the user isn't using FARGATE_SPOT, the launch type defaults
         to FARGATE.
         """
         strategies = values.get("capacity_provider_strategy")
@@ -73,70 +71,29 @@ class ECSTask(Infrastructure):
 
         return values
 
-    # @root_validator
-    # def set_default_network_mode(cls, values):
-    #     """
-    #     If a task definition arn is not linked or the network mode is not set in the
-    #     given task definition the network mode should default to 'awsvpc' when using
-    #     the FARGATE launch mode
-    #     """
-    #     if (
-    #         not values.get("task_definition_arn")
-    #         and not (values.get("task_definition") or {}).get("networkMode")
-    #         and values.get("launch_type") == "FARGATE"
-    #     ):
-    #         values.setdefault("network_mode", "awsvpc")
-    #     return values
-
-    # @root_validator
-    # def check_network_mode_fargate_compatibility(cls, values):
-    #     """
-    #     If using the 'FARGATE' launch mode, the network mode must be 'awsvpc'
-    #     """
-    #     network_mode = (values.get("task_definition") or {}).get("networkMode")
-    #     launch_type = values.get("launch_type")
-    #     if network_mode and network_mode != "awsvpc" and launch_type == "FARGATE":
-    #         raise ValueError(
-    #             f"Network mode {network_mode!r} is not compatible with launch type 'FARGATE'. "
-    #             "'awsvpc' must be used instead."
-    #         )
-    #     return values
-
-    # @root_validator
-    # def set_default_task_family(cls, values):
-    #     """
-    #     If a task definition arn is not linked or the family is not set in the
-    #     given task definition the family should default to 'prefect'
-    #     """
-    #     if not values.get("task_definition_arn") and not values.get(
-    #         "task_definition", {}
-    #     ).get("family"):
-    #         values.setdefault("family", "prefect")
-    #     return values
-
     @sync_compatible
     async def run(self):
-        ecs_client = self.get_ecs_client()
+        ecs_client = self._get_ecs_client()
 
         requested_task_definition = (
-            self.retrieve_task_definition(ecs_client, self.task_definition_arn)
+            self._retrieve_task_definition(ecs_client, self.task_definition_arn)
             if self.task_definition_arn
             else self.task_definition
         ) or {}
         task_definition_arn = requested_task_definition.get("taskDefinitionArn", None)
 
-        task_definition = self.prepare_task_definition(requested_task_definition)
+        task_definition = self._prepare_task_definition(requested_task_definition)
 
         # We must register the task definition if the arn is null or changes were made
         if task_definition != requested_task_definition or not task_definition_arn:
             self.logger.info(f"ECSTask {self.name!r}: Registering task definition...")
             self.logger.debug("\n" + yaml.dump(task_definition))
-            task_definition_arn = self.register_task_definition(
+            task_definition_arn = self._register_task_definition(
                 ecs_client, task_definition
             )
 
         if task_definition.get("networkMode") == "awsvpc":
-            network_config = self.load_vpc_network_config(self.vpc_id)
+            network_config = self._load_vpc_network_config(self.vpc_id)
         else:
             network_config = None
 
@@ -147,30 +104,15 @@ class ECSTask(Infrastructure):
         try:
             result = ecs_client.run_task(**task_run)
         except Exception as exc:
-            self.report_task_run_failure(task_run, exc)
-
-    def report_task_run_failure(self, task_run, exc: Exception) -> None:
-        """
-        Wrap common AWS task run failures with nicer user-facing messages.
-        """
-        # AWS generates exception types at runtime so they must be captured a bit
-        # differently than normal.
-        if "ClusterNotFoundException" in str(exc):
-            cluster = task_run.get("cluster", "default")
-            raise RuntimeError(
-                f"Failed to run ECS task, cluster {cluster!r} not found. "
-                "Confirm that the cluster is configured in your region."
-            ) from exc
-        else:
-            raise
+            self._report_task_run_failure(task_run, exc)
 
     def preview(self) -> str:
         preview = ""
 
-        task_definition_arn = self.task_definition_arn or "<registered-at-runtime>"
+        task_definition_arn = self.task_definition_arn or "<registered at runtime>"
 
         if self.task_definition or not self.task_definition_arn:
-            task_definition = self.prepare_task_definition(self.task_definition or {})
+            task_definition = self._prepare_task_definition(self.task_definition or {})
             preview += "----- Task definition -----\n"
             preview += yaml.dump(task_definition)
             preview += "\n"
@@ -187,10 +129,25 @@ class ECSTask(Infrastructure):
 
         return preview
 
-    def get_ecs_client(self):
+    def _report_task_run_failure(self, task_run, exc: Exception) -> None:
+        """
+        Wrap common AWS task run failures with nicer user-facing messages.
+        """
+        # AWS generates exception types at runtime so they must be captured a bit
+        # differently than normal.
+        if "ClusterNotFoundException" in str(exc):
+            cluster = task_run.get("cluster", "default")
+            raise RuntimeError(
+                f"Failed to run ECS task, cluster {cluster!r} not found. "
+                "Confirm that the cluster is configured in your region."
+            ) from exc
+        else:
+            raise
+
+    def _get_ecs_client(self):
         return self.aws_credentials.get_boto3_session().client("ecs")
 
-    def retrieve_task_definition(self, ecs_client, task_definition_arn: str):
+    def _retrieve_task_definition(self, ecs_client, task_definition_arn: str):
 
         self.logger.info(
             f"ECSTask {self.name!r}: Retrieving task definition {task_definition_arn!r}..."
@@ -200,13 +157,13 @@ class ECSTask(Infrastructure):
         )
         return response["taskDefinition"]
 
-    def register_task_definition(self, ecs_client, task_definition: dict) -> str:
+    def _register_task_definition(self, ecs_client, task_definition: dict) -> str:
         # TODO: Consider including a global cache for this task definition since
         #       registration of task definitions is frequently rate limited
-        response = ecs_client.register_task_definition(**task_definition)
+        response = ecs_client._register_task_definition(**task_definition)
         return response["taskDefinition"]["taskDefinitionArn"]
 
-    def get_prefect_container_definition(
+    def _get_prefect_container_definition(
         self, container_definitions: List[dict]
     ) -> Optional[dict]:
         for container in container_definitions:
@@ -214,7 +171,7 @@ class ECSTask(Infrastructure):
                 return container
         return None
 
-    def prepare_task_definition(self, task_definition: dict) -> dict:
+    def _prepare_task_definition(self, task_definition: dict) -> dict:
         # Prepare the task definition by inferring any defaults and merging overrides
         task_definition = copy.deepcopy(task_definition)
 
@@ -222,12 +179,11 @@ class ECSTask(Infrastructure):
         task_definition.setdefault(
             "containerDefinitions", [{"name": ECS_TASK_RUN_CONTAINER_NAME}]
         )
-        container = self.get_prefect_container_definition(
+        container = self._get_prefect_container_definition(
             task_definition["containerDefinitions"]
         )
         container["image"] = self.image
 
-        # task_definition.setdefault("networkMode", "awsvpc")
         task_definition.setdefault("family", "prefect")
 
         # CPU and memory are required in some cases, retrieve the value to use
@@ -248,7 +204,14 @@ class ECSTask(Infrastructure):
 
             # Only the 'awsvpc' network mode is supported when using FARGATE
             # However, we will not enforce that here if the user has set it
-            task_definition.setdefault("networkMode", "awsvpc")
+            network_mode = task_definition.setdefault("networkMode", "awsvpc")
+
+            if network_mode != "awsvpc":
+                warnings.warn(
+                    f"Found network mode {network_mode!r} which is not compatible with "
+                    "launch type 'FARGATE'. Either use the 'EC2' launch type or the "
+                    "'awsvpc' network mode."
+                )
 
         elif self.launch_type == "EC2":
             # Container level memory and cpu are required when using ec2
@@ -257,7 +220,7 @@ class ECSTask(Infrastructure):
 
         return task_definition
 
-    def prepare_task_run_overrides(self) -> dict:
+    def _prepare_task_run_overrides(self) -> dict:
         overrides = {
             "containerOverrides": [
                 {
@@ -289,7 +252,7 @@ class ECSTask(Infrastructure):
 
         return overrides
 
-    def load_vpc_network_config(self, vpc_id: Optional[str]) -> dict:
+    def _load_vpc_network_config(self, vpc_id: Optional[str]) -> dict:
 
         ec2_client = self.aws_credentials.get_boto3_session().client("ec2")
         vpc_message = "the default VPC" if not vpc_id else f"VPC with ID {vpc_id}"
@@ -329,11 +292,11 @@ class ECSTask(Infrastructure):
             }
         }
 
-    def prepare_task_run(
+    def _prepare_task_run(
         self, network_config: Optional[dict], task_definition_arn: str
     ) -> dict:
         task_run = {
-            "overrides": self.prepare_task_run_overrides(),
+            "overrides": self._prepare_task_run_overrides(),
             "capacityProviderStrategy": self.capacity_provider_strategy,
             "tags": [
                 {"name": key, "value": value} for key, value in self.labels.items()
