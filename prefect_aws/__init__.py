3799be98e5c73421935feca474d956d107e1b673
Michael Adkins <michael@prefect.io>
2022-09-01 12:55:14 -0500

    Add to root __init__

--- prefect_aws/__init__.py
+++ prefect_aws/__init__.py
@@ -1,7 +1,14 @@
 from . import _version
 from .credentials import AwsCredentials, MinIOCredentials
 from .client_parameters import AwsClientParameters
+from .ecs import ECSTask, ECSTaskResult
 
-__all__ = ["AwsCredentials", "AwsClientParameters", "MinIOCredentials"]
+__all__ = [
+    "AwsCredentials",
+    "AwsClientParameters",
+    "MinIOCredentials",
+    "ECSTask",
+    "ECSTaskResult",
+]
 
 __version__ = _version.get_versions()["version"]
