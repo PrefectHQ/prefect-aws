410df931931544c900782682c0649d8a28c59f9d
Michael Adkins <michael@prefect.io>
2022-09-02 16:13:26 -0500

    Remove tasks from root init

--- prefect_aws/__init__.py
+++ prefect_aws/__init__.py
@@ -1,14 +1,11 @@
 from . import _version
 from .credentials import AwsCredentials, MinIOCredentials
 from .client_parameters import AwsClientParameters
-from .ecs import ECSTask, ECSTaskResult
 
 __all__ = [
     "AwsCredentials",
     "AwsClientParameters",
     "MinIOCredentials",
-    "ECSTask",
-    "ECSTaskResult",
 ]
 
 __version__ = _version.get_versions()["version"]
