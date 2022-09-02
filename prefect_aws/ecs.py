b5c92b8ade3ac560d910797a8f75b1f748589dec
Michael Adkins <madkinszane@gmail.com>
2022-09-02 11:24:56 -0500

    Add some nice block metadata
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>

--- prefect_aws/ecs.py
+++ prefect_aws/ecs.py
@@ -112,6 +112,8 @@ class ECSTask(Infrastructure):
     """
 
     _block_type_slug = "ecs-task"
+    _block_type_name = "ECS Task"
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/1jbV4lceHOjGgunX15lUwT/db88e184d727f721575aeb054a37e277/aws.png?h=250" # noqa
 
     type: Literal["ecs-task"] = Field(
         "ecs-task", description="The slug for this task type."
