9b19dda56bd6206cb6133fca8f6be6549a5aa473
desertaxle <desertaxle@users.noreply.github.com>
2022-12-30 00:04:04 +0000

    Updating collection with changes to prefect-collection-template

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -8,6 +8,8 @@ docs_index_path = Path("index.md")
 with open(readme_path, "r") as readme:
     with mkdocs_gen_files.open(docs_index_path, "w") as generated_file:
         for line in readme:
+            if line.startswith("## Resources"):
+                insert_blocks_catalog(generated_file)
             generated_file.write(line)
 
     mkdocs_gen_files.set_edit_path(Path(docs_index_path), readme_path)
