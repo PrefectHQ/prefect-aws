2c6f6eea5befa99774f2ad48921a522e17567967
Michael Adkins <michael@prefect.io>
2023-04-12 12:27:25 -0500

    WIP: Reached making task run request w/ invalid payload

--- prefect_aws/workers/ecs.py
+++ prefect_aws/workers/ecs.py
@@ -1,12 +1,19 @@
+import copy
 from copy import deepcopy
-from typing import Any, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple
+from uuid import UUID
 
 import anyio
 import boto3
 import yaml
 from prefect.server.schemas.core import FlowRun
 from prefect.utilities.asyncutils import run_sync_in_worker_thread
-from prefect.workers.base import BaseJobConfiguration, BaseWorker, BaseWorkerResult
+from prefect.workers.base import (
+    BaseJobConfiguration,
+    BaseVariables,
+    BaseWorker,
+    BaseWorkerResult,
+)
 from pydantic import Field
 
 from prefect_aws import AwsCredentials
@@ -14,12 +21,27 @@ from prefect_aws import AwsCredentials
 # Internal type alias for ECS clients which are generated dynamically in botocore
 _ECSClient = Any
 
+ECS_DEFAULT_CONTAINER_NAME = "prefect"
+ECS_DEFAULT_CPU = 1024
+ECS_DEFAULT_MEMORY = 2048
+ECS_DEFAULT_FAMILY = "prefect"
+ECS_POST_REGISTRATION_FIELDS = [
+    "compatibilities",
+    "taskDefinitionArn",
+    "revision",
+    "status",
+    "requiresAttributes",
+    "registeredAt",
+    "registeredBy",
+    "deregisteredAt",
+]
+
 
 DEFAULT_TASK_DEFINITION_TEMPLATE = """
 containerDefinitions:
 - image: "{{ image }}"
   name: "{{ container_name }}"
-cpu: '1024'
+cpu: '{{1024}}'
 family: prefect
 memory: '2048'
 networkMode: awsvpc
@@ -39,6 +61,9 @@ taskDefinition: "{{ task_definition_arn }}"
 """
 
 
+_TASK_DEFINITION_CACHE: Dict[UUID, str] = {}
+
+
 def _default_task_definition_template() -> dict:
     return yaml.safe_load(DEFAULT_TASK_DEFINITION_TEMPLATE)
 
@@ -53,12 +78,17 @@ class ECSJobConfiguration(BaseJobConfiguration):
     task_run_request: dict = Field(template=_default_task_run_request_template())
 
 
+class ECSVariables(BaseVariables):
+    task_definition_arn: Optional[str] = Field(default=None)
+
+
 class ECSWorkerResult(BaseWorkerResult):
     pass
 
 
 class ECSWorker(BaseWorker):
     type = "ecs"
+    job_configuration = ECSJobConfiguration
 
     async def run(
         self,
@@ -73,14 +103,35 @@ class ECSWorker(BaseWorker):
             self._get_session_and_client, configuration
         )
 
-        task_definition_arn = self._register_task_definition(
-            ecs_client, configuration.task_definition
-        )
+        cached_task_definition_arn = _TASK_DEFINITION_CACHE.get(flow_run.deployment_id)
 
-        task_run_request = deepcopy.copy(configuration.task_run_request)
+        if cached_task_definition_arn:
+            # Read the task definition to see if the cached task definition is valid
+            task_definition = self._retrieve_task_definition(
+                ecs_client, task_definition_arn
+            )
+
+            if not self._task_definitions_equal(
+                task_definition, configuration.task_definition
+            ):
+                # Cached task definition is not valid
+                cached_task_definition_arn = None
+
+        if not cached_task_definition_arn:
+            task_definition_arn = self._register_task_definition(
+                ecs_client, configuration.task_definition
+            )
+
+        # Update the cached task definition ARN to avoid re-registering the task
+        # definition on this worker unless necessary; registration is agressively
+        # rate limited by AWS
+        _TASK_DEFINITION_CACHE[flow_run.deployment_id] = task_definition_arn
+
+        # Prepare the task run request
+        task_run_request = deepcopy(configuration.task_run_request)
         task_run_request["taskDefinition"] = task_definition_arn
 
-        task_run = self._run_task(ecs_client, configuration.task_run_request)
+        task_run = self._create_task_run(ecs_client, configuration.task_run_request)
         return ECSWorkerResult(identifier=task_run)
 
     def _get_session_and_client(
@@ -105,6 +156,20 @@ class ECSWorker(BaseWorker):
         response = ecs_client.register_task_definition(**task_definition)
         return response["taskDefinition"]["taskDefinitionArn"]
 
+    def _retrieve_task_definition(
+        self, ecs_client: _ECSClient, task_definition_arn: str
+    ):
+        """
+        Retrieve an existing task definition from AWS.
+        """
+        self.logger.info(
+            f"{self._log_prefix}: Retrieving task definition {task_definition_arn!r}..."
+        )
+        response = ecs_client.describe_task_definition(
+            taskDefinition=task_definition_arn
+        )
+        return response["taskDefinition"]
+
     def _create_task_run(self, ecs_client: _ECSClient, task_run_request: dict):
         """
         Create a run of a task definition.
@@ -112,3 +177,55 @@ class ECSWorker(BaseWorker):
         Returns the task run ARN.
         """
         return ecs_client.run_task(**task_run_request)["tasks"][0]
+
+    def _task_definitions_equal(self, taskdef_1, taskdef_2) -> bool:
+        """
+        Compare two task definitions.
+
+        Since one may come from the AWS API and have populated defaults, we do our best
+        to homogenize the definitions without changing their meaning.
+        """
+        if taskdef_1 == taskdef_2:
+            return True
+
+        if taskdef_1 is None or taskdef_2 is None:
+            return False
+
+        taskdef_1 = copy.deepcopy(taskdef_1)
+        taskdef_2 = copy.deepcopy(taskdef_2)
+
+        def _set_aws_defaults(taskdef):
+            """Set defaults that AWS would set after registration"""
+            container_definitions = taskdef.get("containerDefinitions", [])
+            essential = any(
+                container.get("essential") for container in container_definitions
+            )
+            if not essential:
+                container_definitions[0].setdefault("essential", True)
+
+            taskdef.setdefault("networkMode", "bridge")
+
+        _set_aws_defaults(taskdef_1)
+        _set_aws_defaults(taskdef_2)
+
+        def _drop_empty_keys(dict_):
+            """Recursively drop keys with 'empty' values"""
+            for key, value in tuple(dict_.items()):
+                if not value:
+                    dict_.pop(key)
+                if isinstance(value, dict):
+                    _drop_empty_keys(value)
+                if isinstance(value, list):
+                    for v in value:
+                        if isinstance(v, dict):
+                            _drop_empty_keys(v)
+
+        _drop_empty_keys(taskdef_1)
+        _drop_empty_keys(taskdef_2)
+
+        # Clear fields that change on registration for comparison
+        for field in ECS_POST_REGISTRATION_FIELDS:
+            taskdef_1.pop(field, None)
+            taskdef_2.pop(field, None)
+
+        return taskdef_1 == taskdef_2
--- tests/workers/test_ecs_worker.py
+++ tests/workers/test_ecs_worker.py
@@ -1,9 +1,20 @@
+import json
+from functools import partial
+from typing import Callable, Dict, List
 from uuid import uuid4
 
 import pytest
+from moto import mock_ec2, mock_ecs, mock_logs
+from moto.ec2.utils import generate_instance_identity_document
 from prefect.server.schemas.core import FlowRun
 
-from prefect_aws.workers.ecs import ECSJobConfiguration, ECSWorker
+from prefect_aws.workers.ecs import (
+    ECS_DEFAULT_CONTAINER_NAME,
+    ECSJobConfiguration,
+    ECSWorker,
+    _default_task_definition_template,
+    _default_task_run_request_template,
+)
 
 
 @pytest.fixture
@@ -11,13 +22,186 @@ def flow_run():
     return FlowRun(flow_id=uuid4())
 
 
+def inject_moto_patches(moto_mock, patches: Dict[str, List[Callable]]):
+    def injected_call(method, patch_list, *args, **kwargs):
+        for patch in patch_list:
+            result = patch(method, *args, **kwargs)
+        return result
+
+    for account in moto_mock.backends:
+        for region in moto_mock.backends[account]:
+            backend = moto_mock.backends[account][region]
+
+            for attr, attr_patches in patches.items():
+                original_method = getattr(backend, attr)
+                setattr(
+                    backend, attr, partial(injected_call, original_method, attr_patches)
+                )
+
+
+def patch_describe_tasks_add_prefect_container(describe_tasks, *args, **kwargs):
+    """
+    Adds the minimal prefect container to moto's task description.
+    """
+    result = describe_tasks(*args, **kwargs)
+    for task in result:
+        task.containers = [{"name": ECS_DEFAULT_CONTAINER_NAME}]
+    return result
+
+
+def patch_run_task(mock, run_task, *args, **kwargs):
+    """
+    Track calls to `run_task` by calling a mock as well.
+    """
+    mock(*args, **kwargs)
+    return run_task(*args, **kwargs)
+
+
+def patch_calculate_task_resource_requirements(
+    _calculate_task_resource_requirements, task_definition
+):
+    """
+    Adds support for non-EC2 execution modes to moto's calculation of task definition.
+    """
+    for container_definition in task_definition.container_definitions:
+        container_definition.setdefault("memory", 0)
+    return _calculate_task_resource_requirements(task_definition)
+
+
+def create_log_stream(session, run_task, *args, **kwargs):
+    """
+    When running a task, create the log group and stream if logging is configured on
+    containers.
+
+    See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/using_awslogs.html
+    """
+    tasks = run_task(*args, **kwargs)
+    if not tasks:
+        return tasks
+    task = tasks[0]
+
+    ecs_client = session.client("ecs")
+    logs_client = session.client("logs")
+
+    task_definition = ecs_client.describe_task_definition(
+        taskDefinition=task.task_definition_arn
+    )["taskDefinition"]
+
+    for container in task_definition.get("containerDefinitions", []):
+        log_config = container.get("logConfiguration", {})
+        if log_config:
+            if log_config.get("logDriver") != "awslogs":
+                continue
+
+            options = log_config.get("options", {})
+            if not options:
+                raise ValueError("logConfiguration does not include options.")
+
+            group_name = options.get("awslogs-group")
+            if not group_name:
+                raise ValueError(
+                    "logConfiguration.options does not include awslogs-group"
+                )
+
+            if options.get("awslogs-create-group") == "true":
+                logs_client.create_log_group(logGroupName=group_name)
+
+            stream_prefix = options.get("awslogs-stream-prefix")
+            if not stream_prefix:
+                raise ValueError(
+                    "logConfiguration.options does not include awslogs-stream-prefix"
+                )
+
+            logs_client.create_log_stream(
+                logGroupName=group_name,
+                logStreamName=f"{stream_prefix}/{container['name']}/{task.id}",
+            )
+
+    return tasks
+
+
+def add_ec2_instance_to_ecs_cluster(session, cluster_name):
+    ecs_client = session.client("ecs")
+    ec2_client = session.client("ec2")
+    ec2_resource = session.resource("ec2")
+
+    ecs_client.create_cluster(clusterName=cluster_name)
+
+    images = ec2_client.describe_images()
+    image_id = images["Images"][0]["ImageId"]
+
+    test_instance = ec2_resource.create_instances(
+        ImageId=image_id, MinCount=1, MaxCount=1
+    )[0]
+
+    ecs_client.register_container_instance(
+        cluster=cluster_name,
+        instanceIdentityDocument=json.dumps(
+            generate_instance_identity_document(test_instance)
+        ),
+    )
+
+
+def create_test_ecs_cluster(ecs_client, cluster_name) -> str:
+    """
+    Create an ECS cluster and return its ARN
+    """
+    return ecs_client.create_cluster(clusterName=cluster_name)["cluster"]["clusterArn"]
+
+
+def describe_task(ecs_client, task_arn, **kwargs) -> dict:
+    """
+    Describe a single ECS task
+    """
+    return ecs_client.describe_tasks(tasks=[task_arn], **kwargs)["tasks"][0]
+
+
+def describe_task_definition(ecs_client, task):
+    return ecs_client.describe_task_definition(
+        taskDefinition=task["taskDefinitionArn"]
+    )["taskDefinition"]
+
+
+@pytest.fixture
+def ecs_mocks(aws_credentials):
+    with mock_ecs() as ecs:
+        with mock_ec2():
+            with mock_logs():
+                session = aws_credentials.get_boto3_session()
+
+                inject_moto_patches(
+                    ecs,
+                    {
+                        # Add a container when describing any task
+                        "describe_tasks": [patch_describe_tasks_add_prefect_container],
+                        # Fix moto internal resource requirement calculations
+                        "_calculate_task_resource_requirements": [
+                            patch_calculate_task_resource_requirements
+                        ],
+                        # Add log group creation
+                        "run_task": [partial(create_log_stream, session)],
+                    },
+                )
+
+                create_test_ecs_cluster(session.client("ecs"), "default")
+
+                # NOTE: Even when using FARGATE, moto requires container instances to be
+                #       registered. This differs from AWS behavior.
+                add_ec2_instance_to_ecs_cluster(session, "default")
+
+                yield ecs
+
+
 async def construct_configuration(**options):
+    options.setdefault("task_definition", _default_task_definition_template())
+    options.setdefault("task_run_request", _default_task_run_request_template())
     return await ECSJobConfiguration.from_template_and_values(
         base_job_template=ECSWorker.get_default_base_job_template(),
         values=options,
     )
 
 
+@pytest.mark.usefixtures("ecs_mocks")
 async def test_container_command(aws_credentials, flow_run):
     configuration = await construct_configuration(
         aws_credentials=aws_credentials, command="prefect version"
